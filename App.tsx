bff5268f830b3cdd4965efbcf4852faa381178d3
HipsterSantos <santoscampos269@gmail.com>
2024-01-10 09:06:43 +0100

    [update] setting up first screen

--- App.tsx
+++ App.tsx
@@ -5,12 +5,15 @@
  * @format
  */
 
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
+  ImageBackground,
   SafeAreaView,
   ScrollView,
-  StatusBar,
+  TouchableOpacity,
   StyleSheet,
   Text,
   useColorScheme,
@@ -24,36 +27,58 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import bgImage from './assets/img/1.jpg';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const styles = StyleSheet.create({
+  mainContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'gray',
+  },
+  backgroundContainer: {
+    height: '90%',
+    marginBottom: 5,
+  },
+  background: {
+    resizeMode: 'cover', // or 'stretch' for different cover options
+    height: '100%',
+  },
+  backgroundImageStyle: {
+    borderEndEndRadius: 80,
+  },
+  overlay: {
+    flex: 1,
+    width: '100%',
+    borderEndEndRadius: 80,
+    backgroundColor: 'rgba(255, 0, 0, 0.5)', // Red overlay with alpha
+    justifyContent: 'center',
+    alignItems: 'left',
+  },
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  btnContainer: {
+    width: '100%',
+    height: '10%',
+    backgroundColor: 'white',
+  },
+
+  touchable: {
+    width: '50%',
+    height: '20%',
+    color: 'white',
+    backgroundColor: 'red',
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 35,
+    width: 160,
+    padding: 20,
+  },
+});
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,57 +87,31 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const handleOnPress = event => {};
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <View style={styles.mainContainer}>
+          <View style={styles.backgroundContainer}>
+            <ImageBackground
+              source={bgImage}
+              style={styles.background}
+              imageStyle={styles.backgroundImageStyle}>
+              <View style={styles.overlay}>
+                <Text style={styles.headerText}>Angola País Grande e Belo</Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={styles.btnContainer}>
+            <TouchableOpacity style={styles.touchable} onPress={handleOnPress}>
+              <Text style={styles.buttonText}>Press Me</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
